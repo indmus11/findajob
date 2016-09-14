2c6facc27c2cbbf26e80cb058225794e8fac54aa
indmus11 <indmus11@htl-shkoder.com>
2016-09-14 10:14:04 +0200

    Update and rename README.md to index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+$url = 'C:\Desktop\indi.jpg';
+
+?>
+
+<html lang="en">
+<head>
+
+<title>This is a test</title>
+
+<style type="text/css">
+body
+{
+background-image:url('<?php echo $url ?>');
+}
+</style>
+
+</head>
+
+<body>
+
+<p>This is a test.</p>
+
+</body>
+</html>
+
